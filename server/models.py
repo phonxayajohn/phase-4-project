34fe2d08e2b95f378dbb2e5e16b998b78849e0c8
John Phonxaya <phonxayajohn@gmail.com>
2023-08-09 21:39:35 -0500

    added models

--- server/models.py
+++ server/models.py
@@ -3,4 +3,39 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+class Store(db.Model, SerializerMixin):
+    __tablename__ = 'stores'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    inventory = db.relationship("Inventory", cascade="all, delete", backref="store")
+
+    def __repr__(self):
+        return f'<Store {self.name}>'
+    
+class Beer(db.Model, SerializerMixin):
+    __tablename__ = 'beers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    brand = db.Column(db.String)
+    style = db.Column(db.String)
+
+    inventory = db.relationship("Inventory", cascade="all, delete", backref="beer")
+
+    def __repr__(self):
+        return f'<{self.name}, {self.brand}, {self.style}>'
+    
+class Inventory(db.Model, SerializerMixin):
+    __tablename__ = 'inventory'
+
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer)
+
+    store_id = db.Column(db.Integer, db.ForeignKey("stores.id"))
+    beer_id = db.Column(db.Integer, db.ForeignKey("beers.id"))
+
+    def __repr__(self):
+        return f"<Inventory {self.store_id}, {self.beer_id}, {self.quantity}>"
